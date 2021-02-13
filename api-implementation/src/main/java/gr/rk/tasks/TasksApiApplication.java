9972d0edf9609ff030cc3f6d27511a3dbbab11ae
rafailkyriakopoulos <rafailkyriakopoulos@gmail.com>
2021-02-14 01:48:26 +0200

    Project initialization with a multimodule structure for OpenAPI

---
+++ api-implementation/src/main/java/gr/rk/tasks/TasksApiApplication.java
@@ -0,0 +1,35 @@
+package gr.rk.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+@SpringBootApplication
+@EnableSwagger2
+public class TasksApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksApiApplication.class, args);
+    }
+
+    @Primary
+    @Bean
+    public SwaggerResourcesProvider swaggerResourcesProvider() {
+        return () -> {
+            SwaggerResource wsResource = new SwaggerResource();
+            // This file must exist in the api-definition module. This bean has access to the resources of the other module due to the pom dependency
+            wsResource.setName("Task API");
+            wsResource.setLocation("/V1/openapi.yaml");
+
+            List<SwaggerResource> resources = List.of(wsResource);
+            return resources;
+        };
+    }
+
+}
---
+++ api-implementation/src/test/java/gr/rk/tasks/TasksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.rk.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
