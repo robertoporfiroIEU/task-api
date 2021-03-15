059aba8d7676eb668eb801b160c577f6b6ae5061
rafailkyriakopoulos <rafailkyriakopoulos@gmail.com>
2021-03-15 02:06:21 +0200

    feature/26

--- api-implementation/src/main/java/gr/rk/tasks/resource/TaskResource.java
+++ api-implementation/src/main/java/gr/rk/tasks/resource/TaskResource.java
@@ -5,9 +5,12 @@ import gr.rk.tasks.V1.models.Assign;
 import gr.rk.tasks.V1.models.Comment;
 import gr.rk.tasks.V1.models.Task;
 import gr.rk.tasks.V1.models.TaskDetail;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -43,7 +46,7 @@ public class TaskResource implements TasksApi {
     }
 
     @Override
-    public ResponseEntity<Page<Task>> getTasks() {
+    public ResponseEntity<Page<Task>> getTasks(@Valid Pageable pageable) {
         List<Task> tasks = List.of(new Task().name("test"));
         return ResponseEntity.ok(new PageImpl<>(tasks));
     }
