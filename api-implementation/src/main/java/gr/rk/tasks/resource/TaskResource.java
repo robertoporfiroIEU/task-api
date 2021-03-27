32a66fe9ed9546896fcad05e2d6ff6c810e02278
rafailkyriakopoulos <rafailkyriakopoulos@gmail.com>
2021-03-27 15:18:02 +0200

    feature/30

--- api-implementation/src/main/java/gr/rk/tasks/resource/TaskResource.java
+++ api-implementation/src/main/java/gr/rk/tasks/resource/TaskResource.java
@@ -3,19 +3,18 @@ package gr.rk.tasks.resource;
 import gr.rk.tasks.V1.api.TasksApi;
 import gr.rk.tasks.V1.models.Assign;
 import gr.rk.tasks.V1.models.Comment;
+import gr.rk.tasks.V1.models.Spectator;
 import gr.rk.tasks.V1.models.Task;
-import gr.rk.tasks.V1.models.TaskDetail;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,28 +22,6 @@ import java.util.UUID;
 @RestController
 @Validated
 public class TaskResource implements TasksApi {
-
-    @Override
-    public ResponseEntity<Assign> addAssign(UUID identifier, @Valid Assign assign) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Comment> addTaskComment(UUID identifier, @Valid Comment comment) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<TaskDetail> getTaskDetail(UUID identifier) {
-        TaskDetail taskDetail = new TaskDetail();
-        taskDetail.setName("test");
-        taskDetail.setIdentifier(UUID.randomUUID());
-        taskDetail.setComments(
-                List.of(new Comment().text("text comment")
-                        .creationDate(LocalDateTime.now().atZone(ZoneId.of("UTC").normalized()).toString()))
-        );
-        return ResponseEntity.ok(taskDetail);
-    }
 
     @Override
     public ResponseEntity<Page<Task>> getTasks(
@@ -61,5 +38,47 @@ public class TaskResource implements TasksApi {
         return ResponseEntity.ok(new PageImpl<>(tasks));
     }
 
+    @Override
+    public ResponseEntity<Task> getTask(UUID identifier) {
 
+        Task task = new Task();
+
+        task.setName("test");
+        task.setIdentifier(UUID.randomUUID());
+        task.setCommentsUrl(ServletUriComponentsBuilder.fromCurrentRequest().toUriString() + "/comments");
+        task.setAssignsUrl(ServletUriComponentsBuilder.fromCurrentRequest().toUriString() + "/assigns");
+        task.setSpectatorsUrl(ServletUriComponentsBuilder.fromCurrentRequest().toUriString() + "/spectators");
+
+        return ResponseEntity.ok(task);
+    }
+
+    @Override
+    public ResponseEntity<Assign> addAssign(UUID identifier, @Valid Assign assign) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Spectator> addSpectator(UUID identifier, @Valid Spectator spectator) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Comment> addTaskComment(UUID identifier, @Valid Comment comment) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Page<Task>> getAssigns(UUID identifier, @Valid Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Page<Task>> getComments(UUID identifier, @Valid Pageable pageable, @Valid String identifier2, @Valid String createdBy) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Page<Task>> getSpectators(UUID identifier, @Valid Pageable pageable) {
+        return null;
+    }
 }
